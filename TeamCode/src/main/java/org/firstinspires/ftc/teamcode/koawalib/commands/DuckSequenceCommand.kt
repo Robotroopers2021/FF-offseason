cfcfa3aa5d8901419864093448bdfbb3d1107a4f
salban <salban.nithilaselvan@gmail.com>
2022-05-05 17:02:09 -0400

    tabun

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/koawalib/commands/DuckSequenceCommand.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/koawalib/commands/DuckSequenceCommand.kt
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.koawalib.commands
 
 import com.asiankoala.koawalib.command.commands.InstantCmd
+import com.asiankoala.koawalib.command.commands.LoopCmd
 import com.asiankoala.koawalib.command.commands.WaitCmd
 import com.asiankoala.koawalib.command.group.SequentialGroup
+import com.asiankoala.koawalib.logger.Logger
 import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.teamcode.koawalib.subsystem.DuckSpinner
 import org.firstinspires.ftc.teamcode.koawalib.subsystem.DuckSpinnerCommand
@@ -11,35 +13,28 @@ import kotlin.math.pow
 
 
 class DuckSequenceCommand (private val duckSpinner: DuckSpinner) : SequentialGroup(
-//    DuckSpinnerCommand(0.25, duckSpinner ),
-//    WaitCmd(0.45),
-//    DuckSpinnerCommand (0.35, duckSpinner ),
-//    WaitCmd(0.5),
-//    DuckSpinnerCommand (0.85, duckSpinner ),
-//    WaitCmd(0.4),
-//    DuckSpinnerCommand (0.0, duckSpinner )
+    InstantCmd(motionTimer::reset),
+    WaitCmd(3.0).
+        deadlineWith(
+            LoopCmd( {duckSpinner.setPower(setDuckSpeed(motionTimer.seconds()))},  duckSpinner)),
+    DuckSpinnerCommand(0.0, duckSpinner)
 ) {
-    private var motionTimer = ElapsedTime()
 
     init {
         addRequirements(duckSpinner)
-        addCommands(
-            InstantCmd(motionTimer::reset),
-            DuckSpinnerCommand(setDuckSpeed(motionTimer.milliseconds()), duckSpinner),
-            WaitCmd(3.0),
-            DuckSpinnerCommand(0.0, duckSpinner)
-        )
     }
+
     override fun end() {
         duckSpinner.setPower(0.0)
     }
 }
 
+private var motionTimer = ElapsedTime()
 
 fun setDuckSpeed(time : Double) : Double{
     var result = 0.0
     if(time > 0 && time<=1) {
-        result = (2/3) * (1.9).pow(time) - (2/4)
+        result = (2.0/3.0) * (1.9).pow(time) - (2.0/4.0)
     } else if (time > 1.0 && time < 1.6 ) {
         result = 0.76
     }
